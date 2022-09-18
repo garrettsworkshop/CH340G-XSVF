fbb614bbc369f9568e0bd85ca8075549be1bc39a
Zane Kaminski <zanekaminski@gmail.com>
2022-09-18 18:31:30 -0400

    Add test files

---
+++ Packager/build_reu.sh
@@ -0,0 +1,2 @@
+gcc Packager.c; ./a.out 384104 012B9043 Instr1.txt Instr2.txt ../REU.svf ../x64/Release/GWUpdate.exe REU.exe
+
---
+++ Packager/build_timedisk.sh
@@ -0,0 +1,3 @@
+gcc Packager.c; ./a.out 799341 071280dd Instr1.txt Instr2.txt ../TimeMachine.svf ../x64/Release/GWUpdate.exe TimeDisk.exe
+
+

5ea93c77058b64a6c38f48495abd6828211113e7
Zane Kaminski <zanekaminski@gmail.com>
2022-09-18 20:32:24 -0400

    Add Windows test build scripts

---
+++ Packager/build_reu.bat
@@ -0,0 +1,1 @@
+..\x64\Release\Packager.exe 384183 012B9043 REU1.txt REU2.txt ..\REU.svf ..\x64\Release\GWUpdate.exe REU.exe
---
+++ Packager/build_timedisk.bat
@@ -0,0 +1,1 @@
+..\x64\Release\Packager.exe 799341 071280dd TimeDisk1.txt TimeDisk2.txt ../TimeMachineFixed.svf ../x64/Release/GWUpdate.exe TimeDisk.exe
